4d1732e2a5fe3bdef89041da55a772faea9e2892
Lunarixus <Nathanb644@hotmail.co.uk>
2021-05-14 19:23:42 +0000

    msm8953-common: Remove unwanted GApps Packages
    Change-Id: Ie4f08459a6004bb16d7dfec7e7a152a42dc0ffdf
    Signed-off-by: RayapKun <rayapkun@gmail.com>
    Signed-off-by: frost. (frostg-012) <frost@nusantararom.org>

--- msm8953.mk
+++ msm8953.mk
@@ -310,6 +310,10 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qcom.post_boot.sh
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # RIL
 PRODUCT_PACKAGES += \
     CarrierConfigOverlayMi8953 \
